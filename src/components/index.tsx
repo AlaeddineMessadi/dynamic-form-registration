b8010a978c86f94be982217b9152ab2085d98540
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-21 17:45:13 +0100

    Create RadioButton component with styles

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "./button";
 import { ButtonsControl } from "./buttons-control";
 import { TextInput } from "./input-text";
+import { RadioButton } from "./radio-button";
 
-export { Button, ButtonsControl, TextInput };
+export { Button, ButtonsControl, TextInput, RadioButton };
---
+++ src/components/radio-button/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.scss";
+
+interface RadioOptionProps {
+  value: string;
+  label: string;
+  name: string;
+  optionId: string;
+}
+
+export const RadioButton: React.FC<RadioOptionProps> = ({
+  value,
+  label,
+  optionId,
+  name,
+}) => {
+  return (
+    <div className="radio-wrapper">
+      <input
+        className="state"
+        type="radio"
+        name={name}
+        id={optionId}
+        value={value}
+      />
+      <label className="label" htmlFor={optionId}>
+        <div className="indicator"></div>
+        <span className="text">{label}</span>
+      </label>
+    </div>
+  );
+};
