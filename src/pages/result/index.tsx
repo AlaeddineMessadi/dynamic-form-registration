7a533d00e9d6b1f7da957a33b5c99de70649a4f1
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-12-22 18:37:30 +0100

    Show data in result page

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -10,15 +10,26 @@ interface IPropsResult {}
 const ResultPage: React.FC<IPropsResult> = () => {
   const data = useSelector((state: IAppState) => state.data);
 
+  const isDataEmpty = Object.keys(data).length === 0;
   return (
     <section className="container">
       <div className="result">
-        <h1>Thank you for registering</h1>
-        <p className="segment result-row">
-          <label className="name">name</label>
-          <label className="value">value is</label>
-        </p>
-        <Link to="/">Registration</Link>
+        {isDataEmpty ? (
+          <>
+            <h1>You need to register first</h1>
+            <Link to="/">Register</Link>
+          </>
+        ) : (
+          <>
+            <h1>Thank you for registering</h1>
+            {Object.keys(data).map((key, index) => (
+              <p className="segment result-row">
+                <label className="name">{key}</label>
+                <label className="value">{data[key]}</label>
+              </p>
+            ))}
+          </>
+        )}
       </div>
     </section>
   );
